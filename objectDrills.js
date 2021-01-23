824fc16f1cf862568ba0311e560f03705ad9e114
Nathan Leung <nath.leung@gmail.com>
2021-01-23 03:24:32 -0800

    cracking the code

--- objectDrills.js
+++ objectDrills.js
@@ -50,13 +50,44 @@ let person4 = {
 
 let people = [ person1, person2, person3, person4] 
 
-for (indiv in people){
-  if (!people[indiv].boss){
-    console.log(`${people[indiv].jobTitle} ${people[indiv].name} doesn't report to anybody. `);
+function role (people){
+  for (indiv in people){
+    if (!people[indiv].boss){
+      console.log(`${people[indiv].jobTitle} ${people[indiv].name} doesn't report to anybody. `);
+    }
+    else
+      console.log(`${people[indiv].jobTitle} ${people[indiv].name} report to ${people[indiv].boss}. `)
   }
-  else
-    console.log(`${people[indiv].jobTitle} ${people[indiv].name} report to ${people[indiv].boss}. `)
 }
 
 // console.log(people);
+// role(people)
 
+function decode(code){
+let cipher = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+if (cipher.hasOwnProperty(code[0])){
+
+  return code[cipher[code[0]]]
+}
+else{
+  return ' '
+}
+}
+
+function decodeWords(string){
+let accum = ''
+  string = string.split(' ')
+  for (x in string ){
+    accum += decode(string[x])
+  }
+  return accum
+}
+
+console.log(decodeWords('cycle cycle cycle bite aaron'))
+// console.log(decode('cycle'))
